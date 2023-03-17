83e705ea41e1b66648576de458c4dd3592afa541
Hiroya-W <hyuyukun@gmail.com>
2023-03-18 03:41:04 +0900

    :sparkles: Get recommended orgs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { ogpImage } from "./ogp";
+import { getTop3RecommendedOrgs } from "./query";
 
 export type Env = {
-  OGP: KVNamespace;
+  DB: D1Database;
+  OGP_KV: KVNamespace;
 };
 
 const app = new Hono<{ Bindings: Env }>();
@@ -12,11 +14,15 @@ const kvId = (userId: string) => `ogp-${userId}`;
 app.get("/", async (ctx) => {
   const userId = ctx.req.query("uid");
   if (!userId) return ctx.status(404);
+});
+
+app.get("/", async (ctx) => {
+  const userId = ctx.req.query("uid");
+  if (!userId) return ctx.status(404);
 
   ctx.header("Cache-Control", "max-age=3600");
-
   // キャッシュからバイナリで取得
-  const ogpCache = await ctx.env.OGP.get(kvId(userId), {
+  const ogpCache = await ctx.env.OGP_KV.get(kvId(userId), {
     type: "arrayBuffer",
   });
   if (ogpCache) {
@@ -24,12 +30,13 @@ app.get("/", async (ctx) => {
   }
 
   // キャッシュに無ければ生成
-  const ogp = await ogpImage(userId);
-  await ctx.env.OGP.put(kvId(userId), ogp, {
-    expirationTtl: 3600,
-  });
+  return ctx.json(await getTop3RecommendedOrgs(ctx.env.DB, userId));
+  // const ogp = await ogpImage(userId);
+  // await ctx.env.OGP_KV.put(kvId(userId), ogp, {
+  //   expirationTtl: 3600,
+  // });
 
-  return ctx.body(ogp);
+  // return ctx.body(ogp);
 });
 
 export default app;
