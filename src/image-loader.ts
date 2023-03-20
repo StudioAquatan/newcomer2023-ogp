7240db8f390965cb3b98b906682e4666df024651
Hiroya-W <hyuyukun@gmail.com>
2023-03-20 13:34:09 +0900

    :sparkles: Add function to replace to imgix

---
+++ src/image-loader.ts
@@ -0,0 +1,50 @@
+import { Env } from ".";
+import { OgpOrg } from "./query";
+
+type ImageLoaderProps = {
+  env: Env;
+  src: string;
+  width: number;
+  quality: number;
+};
+
+export function imgixLoader({ env, src, width, quality }: ImageLoaderProps) {
+  const newtPrefix = env.NEWT_ROOT ?? "newt/";
+  const assetsPrefix = env.RESOURCE_URL ?? "assets/";
+  if (src.startsWith(newtPrefix)) {
+    src = src.replace(newtPrefix, "newt/");
+  } else if (src.startsWith(assetsPrefix)) {
+    src = src.replace(assetsPrefix, "assets");
+  } else {
+    return src;
+  }
+  return `https://${env.IMGIX_DOMAIN}/${src}?auto=format&fit=max&w=${width}&width=${width}&q=${quality}`;
+}
+
+export default function replaceImgixUrl({
+  env,
+  orgs,
+  width = 80,
+  quality = 75,
+}: {
+  env: Env;
+  orgs: OgpOrg[];
+  width?: number;
+  quality?: number;
+}) {
+  return orgs.map((org) => {
+    return {
+      ...org,
+      logo: {
+        src: imgixLoader({
+          env,
+          src: org.logo.src,
+          width: width,
+          quality: quality,
+        }),
+        width: width,
+        height: width, // ?
+      },
+    };
+  });
+}
